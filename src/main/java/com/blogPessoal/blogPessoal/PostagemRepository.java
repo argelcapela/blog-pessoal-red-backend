2e66886f71308abde332c3f5b3f5c424e26243f6
argelcapela <argelcapeladossantos@gmail.com>
2021-09-28 19:05:17 -0300

    Starting gen_blog_pessoal!

---
+++ src/main/java/com/blogPessoal/blogPessoal/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.blogPessoal.blogPessoal;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+}
