a61115751c65b9a027c92c8cf710c86f900dcc3f
argelcapela <argelcapeladossantos@gmail.com>
2021-12-07 16:13:46 -0300

    a

--- src/main/java/com/argelcapela/blog_pessoal_red_backend/model/Postagem.java
+++ src/main/java/com/argelcapela/blog_pessoal_red_backend/model/Postagem.java
@@ -30,7 +30,7 @@ public class Postagem {
 		@NotNull	// não aceita vazio
 		private String texto;
 		private String imagem;
-		//@NotNull	// não aceita vazio
+		
 		private Date data = new java.sql.Date(System.currentTimeMillis()); // pega o horário do sistema no momento do cadastro
 		@ManyToOne // cria uma chave estrangeira/ define que essa classe é o muitos da relação. ("uma postagem só pode ter um tema, porém um tema pode ter muitas postagens!")
 		@JsonIgnoreProperties("postagens") // evita a recursividade infinita entre postagem e tema!!
